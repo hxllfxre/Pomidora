2a44088c114db0ec00c544e34e75b5b4f2ae1a9d
defrxst <growtopiastfu123@proton.me>
2023-09-17 00:19:50 +0000

    added ringtone

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,40 @@
 <script lang="ts">
+  let button
   let timer
-  let ticking
+  let ticking : Boolean = false
   let minutes
   let seconds
+  let secondsV = 60
+  let minutesV = 24
 
   function time() {
+    if(!ticking) {
+    ticking = true
+      button.innerHTML = 'Stop Timer'
+      setInterval(function() {
+        secondsV-=1
+        if(secondsV < 10) {
+          seconds.innerHTML = `0${secondsV}`
+        } else {
+          seconds.innerHTML = secondsV
+        }
+
+        if(secondsV == 0) {
+          secondsV = 60
+          minutesV-=1
+        }
+        minutes.innerHTML = `${minutesV}:`
+
+        // check if all time has passed
+        if(minutesV == 0) {
+          //alert
+        }
+      },1000) 
+    }
+    if(ticking) {
+      clearInterval
+    }
   }
-  setInterval(time, 1000)
 </script>
 
 <main class="w-screen h-screen flex items-center justify-center flex-col bg-black">
@@ -15,7 +43,7 @@
     <h1 bind:this={minutes} class="text-3xl font-extrabold text-white">25:</h1>
     <h1 bind:this={seconds} class="text-3xl font-extrabold text-white">00</h1>
   </div>
-  <button on:click={()=>time()} class="text-white border-white border-2 p-5 rounded-3xl mt-14">
+  <button on:click={()=>time()} bind:this={button} class="text-white border-white border-2 p-5 rounded-3xl mt-14">
     Start Timer
   </button>
 </main>
