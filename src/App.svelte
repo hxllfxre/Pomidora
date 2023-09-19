88a08d1402d416e0d632da316f9c3b1319f83aca
defrxst <growtopiastfu123@proton.me>
2023-09-19 18:29:28 +0000

    added break button

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
   let seconds : any
   let secondsV = 60
   let minutesV = 24
+  let breakButton : any
   var ringtone = new Audio('/ringtone.mp3')
   var interval : any
 
@@ -61,7 +62,15 @@
     <h1 bind:this={minutes} class="text-3xl font-extrabold text-white">25:</h1>
   <h1 bind:this={seconds} class="text-3xl font-extrabold text-white">00</h1>
   </div>
-  <button on:click={()=>time()} bind:this={button} class="text-white border-white border-2 p-5 rounded-3xl mt-14">
+
+  <div class="flex">
+    <button bind:this={breakButton} class="text-white border-white border-2 p-5 rounded-3xl mt-5 w-36 font-extrabold">
+      Break
+    </button>
+  </div>
+
+  <button on:click={()=>time()} bind:this={button} class="text-white border-white border-2 p-5 rounded-3xl mt-8 w-36 font-extrabold">
     Start Timer
   </button>
+
 </main>
